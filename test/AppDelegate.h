8a098a21bfb675955ffe551e0f277e3d2534909a
KeSen <317331820@qq.com>
2015-11-25 09:15:10 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by KeSen on 15/11/25.
+//  Copyright © 2015年 KeSen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
